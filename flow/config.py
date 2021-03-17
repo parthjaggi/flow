95f7ab047cc8ddf7a63073c2945d7fae10d8b024
Ilia Smirnov <iliathesmirnov@gmail.com>
2021-03-17 15:43:36 -0400

    Additional entries in flow/config

--- flow/config.py
+++ flow/config.py
@@ -18,10 +18,29 @@ AWS_ACCESS_SECRET = os.environ.get("AWS_ACCESS_SECRET", None)
 
 AWS_S3_PATH = "s3://bucket_name"
 
+
+# ===========================================================================
+# =========================== Aimsun config  ================================
+
 # path to the Aimsun_Next main directory (required for Aimsun simulations)
 AIMSUN_NEXT_PATH = os.environ.get("AIMSUN_NEXT_PATH", None)
 
 
-# path to the aimsun_flow environment's main directory (required for Aimsun
-# simulations)
-AIMSUN_SITEPACKAGES = os.environ.get("AIMSUN_SITEPACKAGES", None)
+# Constants for the TCP connection to Aimsun
+HOST = 'localhost'
+# The PORT is now a random integer between 1000 and 9999 inclusive
+
+# Number of characters in a packet when sending a string
+PACKET_SIZE = 256
+
+# Signals for communicating the status of a transfer to the client
+TRANSFER_DONE      = b'Wolf0'
+TRANSFER_CONTINUE  = b'Wolf1'
+
+# Standard length for a bytestring sent as a Status Response
+STATRESP = b'StatResp'
+STATRESP_LEN = 32
+
+# Client identifiers
+NETWORK_LOAD_ID   = b'load.py_and_generate.py'
+RUN_API_ID        = b'run.py_and_api.py'
